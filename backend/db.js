7f908abd7eeb5f772bee9bb3687dc847b0471f3a
Arun Dharekar <arundharekar2004@gmail.com>
2025-07-08 00:37:20 +0530

    Initial commit - MERN food delivery app

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 // const mongoDbClient = require("mongodb").MongoClient
-const mongoURI = 'mongodb://<!#@!#@>:"!@!@"@mernclustedsfhcbshdbf$##################,merncluster-shard-00-01.d1d4z.mongodb.net:27017,merncluster-shard-00-02.d1d4z.mongodb.net:27017/Customer?ssl=true&replicaSet=atlas-eusy5p-shard-0&authSource=admin&retryWrites=true&w=majority' // Customer change url to your db you created in atlas
-
+const mongoURI = 'mongodb://ArunDharekar:Arun2004@ac-5cevgnj-shard-00-00.lmcnjko.mongodb.net:27017,ac-5cevgnj-shard-00-01.lmcnjko.mongodb.net:27017,ac-5cevgnj-shard-00-02.lmcnjko.mongodb.net:27017/goFood?ssl=true&replicaSet=atlas-38mtts-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0'
 module.exports = function (callback) {
     mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
         // mongoDbClient.connect(mongoURI, { useNewUrlParser: true }, async(err, result) => {
